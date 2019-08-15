d09e3dd081dfca48d771dacaf06e83a771022aa4
derek Xue <xuehaige@gmail.com>
2019-08-15 10:35:00 +0800

    addedStudyNotes

--- wp-content/plugins/alecaddd-plugin/alecaddd-plugin.php
+++ wp-content/plugins/alecaddd-plugin/alecaddd-plugin.php
@@ -24,18 +24,13 @@ class AlecadddPlugin
     //protected
         //can be accessed only within the class itself or extension of that class
     //private
-        //can be access by the class itself , not even it's subclass
+        //can be access by the class itself , not even it's sub class
 
-    function __construct(){
-        $this->printstf();
-        echo '1231231232';   
+     static function register(){
+        add_action('admin_enqueue_scripts',array('AlecadddPlugin','enqueue'));
     }
 
-    function register(){
-        add_action('admin_enqueue_scripts',array($this,'enqueue'));
-    }
-
-    protected function create_post_type(){
+    function create_post_type(){
         add_action('init',array($this,'custom_post_type'));
     }
 
@@ -44,9 +39,6 @@ class AlecadddPlugin
        flush_rewrite_rules();
     }
 
-    private function printstf(){
-        echo 'what ever';
-    }
 
     function deactivate(){
        
@@ -60,7 +52,7 @@ class AlecadddPlugin
         register_post_type('book',['public'=>true,'label'=>'Books']); //register customer post type
     }
 
-    function enqueue(){
+    static function enqueue(){
         //enqueue all our scripts
         wp_enqueue_style('mypluginstyle',plugins_url('/assets/mystyle.css',__FILE__));
         wp_enqueue_script('mypluginscript',plugins_url('/assets/myscript.js',__FILE__));
@@ -68,31 +60,19 @@ class AlecadddPlugin
 
 }
 
-class Secondclass extends AlecadddPlugin
-{
-    function __construct(){
-        echo 'addssdadssdds';   
-    }
-
-    function register_post_type(){
-        
-        $this->create_post_type();
-    }
-}
 
 if(class_exists('AlecadddPlugin')){
-    $alecadddPlugin=new AlecadddPlugin('Alecadd get instantiated!!!!!!!!!!!!!!!!'); //instantiate
-    $secondClass=new Secondclass();
-    
-    $secondClass->register_post_type();
-    $alecadddPlugin->register(); //php function call
+    // $alecadddPlugin=new AlecadddPlugin('Alecadd get instantiated!!!!!!!!!!!!!!!!'); //instantiate
+    // $alecadddPlugin->register(); //php function call
+
+    AlecadddPlugin::register();
 }
 
-// activation
-register_activation_hook(__FILE__,array($alecadddPlugin,'activate'));
+// // activation
+// register_activation_hook(__FILE__,array($alecadddPlugin,'activate'));
 
-//deactivation
-register_deactivation_hook(__FILE__,array($alecadddPlugin,'deactivate'));
+// //deactivation
+// register_deactivation_hook(__FILE__,array($alecadddPlugin,'deactivate'));
 
-// //uninstall
-// register_uninstall_hook(__FILE__,array($alecadddPlugin,'uninstall'));
+// // //uninstall
+// // register_uninstall_hook(__FILE__,array($alecadddPlugin,'uninstall'));
